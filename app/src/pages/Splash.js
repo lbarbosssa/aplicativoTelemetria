343487306b9931ad975851317ca7c76434ee4283
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-08 15:46:30 -0200

    Barrinha no tamanho certo da tela

--- app/src/pages/Splash.js
+++ app/src/pages/Splash.js
@@ -2,35 +2,40 @@ import React, { Component } from "react";
 
 import { View, StyleSheet, Image, Text, Animated, Easing, Dimensions } from "react-native";
 
+const widthScrenn = Dimensions.get('window')
+
 export default class Splash extends Component {
     // componentWillMount(){
-    //     setInterval(() => {
-    //         this.props.navigation.navigate('Login')
-    //     }, 2000)
+    //     
     // }
 
     componentWillMount() {
-        this.animatedValue = new Animated.Value(1)
+        this.barrinhaAnimada = new Animated.Value(0)
+        setInterval(() => {
+            this.props.navigation.navigate('Login')
+        }, 1500)
     }
     componentDidMount() {
-        Animated.timing(this.animatedValue, {
-            toValue: 400,
-            duration: 1000,
+        Animated.timing(this.barrinhaAnimada, {
+            toValue: widthScrenn.width,
+            duration: 1500,
             easing: Easing.bounce
         }).start()
     }
 
     render() {
-        const barrinha = { width: this.animatedValue}
+        const barrinha = { width: this.barrinhaAnimada }
+
         return (
             <View style={styles.container}>
                 <Animated.View style={[styles.box, barrinha]} />
+
             </View>
         )
     }
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         backgroundColor: '#003D7B',
         flex: 1,
