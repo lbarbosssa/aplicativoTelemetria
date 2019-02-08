2349faefd90256465aabfd20d4a32b3deafbcd22
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-08 12:42:30 -0200

    Finalizando tela login

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { View, StyleSheet, TextInput, Image } from "react-native";
+import { View, StyleSheet, TextInput, Image, Text, TouchableOpacity } from "react-native";
 
 export default class LoginPage extends Component {
   render() {
@@ -10,12 +10,21 @@ export default class LoginPage extends Component {
         style={styles.image}
         source={{uri: 'https://www.braspress.com/wp-content/themes/braspress/img/braspress-logo.png'}} 
         />
+        <Text style={styles.textBTU} >Telemetria</Text>
         <TextInput
-          style={styles.input}
+          style={[styles.input, styles.textGlobal]}
           placeholder="Senha telemetria"
           placeholderTextColor="#EB8822"
           keyboardType="numeric"
+          secureTextEntry
+          maxLength={6}
         />
+        <TouchableOpacity
+         style={styles.button}
+         
+       >
+         <Text style={[styles.textButton, styles.textGlobal]}> Entrar </Text>
+       </TouchableOpacity>
       </View>
     );
   }
@@ -34,11 +43,34 @@ const styles = StyleSheet.create({
     padding: 15,
     width: "90%",
     borderRadius: 6,
-    color: "#EB8822"
   },
   image: {
     //aspectRatio: 1,
     width: 270,
-    height: 100
+    height: 40
+  },
+  textBTU: {
+    fontSize: 20,
+    color: "#EB8822",
+    paddingBottom: 5,
+    fontFamily: 'Bitter-Italic',
+    alignSelf: 'flex-end',
+    paddingRight: 35
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: "#003366",
+    width: "90%",
+    borderRadius: 6,
+    
+  },
+  textButton: {
+    padding: 17,
+    textAlign: "center",   
+  },
+  textGlobal: {
+    color: "#EB8822",
+    fontSize: 18,
+    fontFamily: 'Bitter-Regular',
   }
 });
