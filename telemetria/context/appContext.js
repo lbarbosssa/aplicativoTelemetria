c3816579b75ad63ffd3b79bf76948e8b84dcad75
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-19 18:01:20 -0300

    Dados reais no home incio

--- telemetria/context/appContext.js
+++ telemetria/context/appContext.js
@@ -30,16 +30,14 @@ export class AppProvider extends React.Component{
         }
         axios.post('http://172.16.75.99:8080/v1/telemetria/login',`driverCode=${driverCode}`, {header: headers})
             .then(data => {
-                console.log(data)
-                this.setState({ driverCode, logar: true, loginMsg: 'Ok' })
+                //console.log(data.data)
+                this.setState({ driverCode, logar: true, loginMsg: 'Ok', objHomeInfo: data.data })
+                console.log(this.state.objHomeInfo)
                 nav.navigate('Home') 
             }).catch(error =>{
                 console.log('Deu rum: ', error)
                 this.setState({ logar: false, loginMsg: 'Senha incorreta' }) 
-            })
-     
-     
-   
+            }) 
     }
      
     render(){
@@ -49,6 +47,7 @@ export class AppProvider extends React.Component{
                    driverCode: this.state.driverCode,
                    login: this.login,
                    loginMsg: this.state.loginMsg,
+                   objHomeInfo: this.state.objHomeInfo
                }}>
                 {this.props.children}
                                        
--- telemetria/src/components/HomeInfo.js
+++ telemetria/src/components/HomeInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {ScrollView} from 'react-native'
+import { ScrollView } from 'react-native'
 import {
     Content,
     Card,
@@ -11,31 +11,37 @@ import {
     Icon,
 } from "native-base";
 import Estilo from "../Estilo";
+import { AppConsumer } from "../../context/appContext";
 
 class HomeInfo extends Component {
-    
+
     render() {
         return (
-            <Content style={Estilo.container}>
-                <Card>
-                <CardItem header>
-                    <Text style={Estilo.cardTextHeader}>Bem Vindo!</Text>
-                </CardItem>
-                    <CardItem>
-                        <Body>
-                            <Text style={Estilo.cardText} numberOfLines={1}>Fabio Assunção</Text>
-                            <Text style={Estilo.cardText} numberOfLines={1}>12345678</Text>
-                            <Text style={Estilo.cardText} numberOfLines={1}>Poa Transferência</Text>
-                            <Text style={Estilo.cardText} numberOfLines={1}>5 Km/l </Text>
-                        </Body>
-                    </CardItem>
-                    <CardItem footer>
-                        <Text>
-                            Seu nivel de direção está a baixo da média!
+            <AppConsumer>
+                {(context) => (
+                    <Content style={Estilo.container}>
+                        <Card>
+                            <CardItem header>
+                                <Text style={Estilo.cardTextHeader}>Bem Vindo!</Text>
+                            </CardItem>
+                            <CardItem>
+                                <Body>
+                                    <Text style={Estilo.cardText} numberOfLines={1}>{context.objHomeInfo.driverName}</Text>
+                                    <Text style={Estilo.cardText} numberOfLines={1}>{context.objHomeInfo.idFuncionario}</Text>
+                                    <Text style={Estilo.cardText} numberOfLines={1}>Poa Transferência</Text>
+                                    <Text style={Estilo.cardText} numberOfLines={1}>5 Km/l </Text>
+                                    <Text style={Estilo.cardText} numberOfLines={1}>{context.objHomeInfo.totalKmRodados} Rodados</Text>
+                                </Body>
+                            </CardItem>
+                            <CardItem footer>
+                                <Text>
+                                    Seu nivel de direção está a baixo da média!
                         </Text>
-                    </CardItem>
-                </Card>
-            </Content>
+                            </CardItem>
+                        </Card>
+                    </Content>
+                )}
+            </AppConsumer>
 
         )
     }
